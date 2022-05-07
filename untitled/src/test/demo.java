919c287b438b48a1d8a1f9abc8f82436cb9b3713
chengyu22 <209545769@qq.com>
2022-05-07 16:26:34 +0800

    Initial commit

---
+++ untitled/src/test/demo.java
@@ -0,0 +1,18 @@
+package test;
+
+        import java.util.Date;
+
+/**
+ * @author bob
+ * @data 2022/5/6-11:15
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("hello world!!!!");
+        Date date1 = new Date();
+        date1.setTime(2021);
+        System.out.println(date1);
+
+    }
+
+}
